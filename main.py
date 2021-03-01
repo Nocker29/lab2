dc2f8c64d6d637930b3c05a8a479f2732f876bc1
Nocker29 <49701710+Nocker29@users.noreply.github.com>
2021-03-01 14:30:11 +0100

    zad1

---
+++ main.py
@@ -0,0 +1,10 @@
+# zad1
+# ulubione_filmy=["Avengers", "Nowy początek", "Kryptonim U.N.C.L.E.", "Jason Bourne", "Coś", "Spider-man"]
+# print(ulubione_filmy)
+# ulubione_filmy.reverse()
+# print(ulubione_filmy)
+# ulubione_filmy.insert(5,"Thor")
+# ulubione_filmy.insert(6,"Split")
+# ulubione_filmy.insert(7,"Ugotowany")
+# ulubione_filmy.insert(8,"Tożsamość zdrajcy")
+# print(ulubione_filmy)
