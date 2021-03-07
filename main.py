099dbc5457101f5bdab9090f6a9a94407991e557
Nocker29 <49701710+Nocker29@users.noreply.github.com>
2021-03-07 17:18:24 +0100

    zad 2,3 i 4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# zad1
+# # zad1
 # ulubione_filmy=["Avengers", "Nowy początek", "Kryptonim U.N.C.L.E.", "Jason Bourne", "Coś", "Spider-man"]
 # print(ulubione_filmy)
 # ulubione_filmy.reverse()
@@ -8,3 +8,30 @@
 # ulubione_filmy.insert(7,"Ugotowany")
 # ulubione_filmy.insert(8,"Tożsamość zdrajcy")
 # print(ulubione_filmy)
+#
+# # zad2
+# slownik_filmow={'Sci-fi 1':ulubione_filmy[0],'Sci-fi 2':ulubione_filmy[4],'Sci-fi 3':ulubione_filmy[5],'Sci-fi 4':ulubione_filmy[6],'Sci-fi 5':ulubione_filmy[9]}
+# slownik_filmow['Akcji 1']=ulubione_filmy[2]
+# slownik_filmow['Akcji 2']=ulubione_filmy[3]
+# slownik_filmow['Akcji 3']=ulubione_filmy[8]
+# slownik_filmow['Komedio dramat 1']=ulubione_filmy[7]
+# slownik_filmow['Horror 1']=ulubione_filmy[1]
+# print(slownik_filmow)
+#
+# # zad3
+# przedmioty={'WD':'Wizualizacja danych','CAD':'CAD Komputerowe wspomaganie programowania','Rric':'Rachunek różniczkowy i całkowy'}
+# przedmioty['Ang']='Język angielski'
+# przedmioty['Emd']='Elementy matematyki dyskretnej'
+# przedmioty['PS']='Programowanie strukturalne'
+# przedmioty['Kul kres']='Kultura kresów północno-wschodnich i jej kontynuacja'
+# print(przedmioty)
+# suma=0
+# for x in przedmioty:
+#     suma+=1
+# print(suma)
+#
+# # zad4
+# from math import *
+# a=int(input('Wprowadź liczbę: '))
+# a=pow(a,a)
+# print(a)
